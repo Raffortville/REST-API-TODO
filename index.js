d844d15ba92f15a31510a77a580f4d64e837ece9
Raffortville <raffihaycan@gmail.com>
2020-12-25 13:19:36 +0400

    update

--- index.js
+++ index.js
@@ -10,11 +10,21 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 
+   /* https://vast-plains-10523.herokuapp.com/*/
+
+
+  const player = { name:"Payet", position :"milieu"} 
+
 app.get('/', (req,res) => {
 
     return res.send("test de l'API")
 })
 
+
+app.get('/player',(req,res) => {
+
+    res.json(player)
+})
 
 app.use('/tasks/', taskRoute)
 
